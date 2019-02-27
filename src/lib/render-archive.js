0921683bb42a63401ce249ad89c5c1f9efb9fb97
Sergio Xalambrí <hello@sergiodxa.com>
2019-02-27 10:46:05 -0500

    Create functino to render the archive page

---
+++ src/lib/render-archive.js
@@ -0,0 +1,52 @@
+const { renderStylesToString } = require("emotion-server");
+const { jsx } = require("@emotion/core");
+const ReactDOMServer = require("react-dom/server");
+const matter = require("gray-matter");
+const { join } = require("path");
+
+const { makeDir, writeFile } = require("./fs");
+
+const ArchivePage = require("../components/archive");
+const Document = require("../components/document");
+
+function formatURL(path) {
+  return path.slice(1, path.length - 4) + "/";
+}
+
+function extractMetadata(article) {
+  const extracted = matter(article.content);
+  return Object.assign(
+    extracted.data,
+    { path: formatURL(article.path) },
+    extracted.data.tags && typeof extracted.data.tags === "string"
+      ? {
+          tags: extracted.data.tags.split(/,\s?/)
+        }
+      : {}
+  );
+}
+
+async function writeContent(html) {
+  const finalPath = join(process.cwd(), "./public", "/articles", "/");
+  await makeDir(finalPath);
+  await writeFile(join(finalPath, "index.html"), html, "utf8");
+}
+
+async function render(config, articles) {
+  console.log("Rendering archive of articles");
+  const links = articles.map(article => formatURL(article.path));
+  const metadatas = articles.map(article => extractMetadata(article));
+
+  const html = renderStylesToString(
+    ReactDOMServer.renderToStaticMarkup(
+      jsx(
+        Document,
+        { config, links },
+        jsx(ArchivePage, { config, articles: metadatas })
+      )
+    )
+  );
+  await writeContent(html);
+}
+
+module.exports = render;
