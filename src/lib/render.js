73b5bd1175b7e8a7cd4909235ff76747a9f0d55f
Sergio Xalambrí <hello@sergiodxa.com>
2019-02-27 12:15:51 -0500

    Use Stream rendering with a promise wrapper for async rendering

--- src/lib/render.js
+++ src/lib/render.js
@@ -1,9 +1,10 @@
-const ReactDOMServer = require("react-dom/server");
-const { renderStylesToString } = require("emotion-server");
+const { renderToStaticNodeStream } = require("react-dom/server");
+const { renderStylesToNodeStream } = require("emotion-server");
 const { jsx } = require("@emotion/core");
 const getHrefs = require("get-hrefs");
 const parseURL = require("parse-url");
 const { join } = require("path");
+const del = require("del");
 
 const Layout = require("../components/layout");
 const Document = require("../components/document");
@@ -11,6 +12,21 @@ const Document = require("../components/document");
 const { writeFile, makeDir } = require("./fs");
 
 const isLocalURL = url => !parseURL(url).resource;
+
+function renderContent(ui) {
+  return new Promise(resolve => {
+    let html = "";
+    const stream = renderToStaticNodeStream(ui).pipe(
+      renderStylesToNodeStream()
+    );
+    stream.on("data", chunk => {
+      html += chunk;
+    });
+    stream.on("end", () => {
+      resolve(html);
+    });
+  });
+}
 
 async function render(article, config) {
   const tmpPath = join("./.tmp", article.path).replace("mdx", "js");
@@ -21,21 +37,16 @@ async function render(article, config) {
 
   const Component = require(join(process.cwd(), tmpPath));
 
-  const content = renderStylesToString(
-    ReactDOMServer.renderToStaticMarkup(
-      jsx(Layout, { ...article, config }, jsx(Component))
-    )
+  const content = await renderContent(
+    jsx(Layout, { ...article, config }, jsx(Component))
   );
 
-  // await unlink(tmpPath);
-  // await del("./.tmp/**");
+  await del("./.tmp/**");
 
   const links = getHrefs(content).filter(isLocalURL);
 
-  const html = renderStylesToString(
-    ReactDOMServer.renderToStaticMarkup(
-      jsx(Document, { ...article, config, links, content })
-    )
+  const html = await renderContent(
+    jsx(Document, { ...article, config, links, content })
   );
 
   return { content: html, path: article.path };
