058330d5ce313cdc9bf870e5af3ebf24911d7d65
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-03 00:01:45 -0500

    Create lib to get metadata from content and cache it in memory

---
+++ src/lib/get-meta.js
@@ -0,0 +1,20 @@
+const matter = require("gray-matter");
+
+const cache = new Map();
+
+function getMeta(file) {
+  if (cache.has(file.path)) return cache.get(file.path);
+  const extracted = matter(file.content);
+  const data = Object.assign(
+    extracted.data,
+    extracted.data.tags && typeof extracted.data.tags === "string"
+      ? {
+          tags: extracted.data.tags.split(/,\s?/)
+        }
+      : {}
+  );
+  cache.set(file.path, { data, content: extracted.content });
+  return { data, content: extracted.content };
+}
+
+module.exports = getMeta;
