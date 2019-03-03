87f6974aa3f49709a07c50bb93e4d56c1a300f11
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-03 00:01:55 -0500

    Remove commented code

--- src/lib/get-articles.js
+++ src/lib/get-articles.js
@@ -1,14 +1,10 @@
 const globby = require("globby");
 const { readFile } = require("./fs");
 
-// import updatedFiles from "./updated-files";
-
 const regex = ["./articles/**/*.mdx"];
 
 async function getArticles() {
   const paths = await globby(regex);
-  // const updated = await updatedFiles();
-  // filter somehow
   return await Promise.all(
     paths.map(async path => ({
       content: await readFile(path, "utf8"),
