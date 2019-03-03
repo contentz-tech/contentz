b3c74eabd1fb9a94fd79f0230c8be967c4e9245d
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-03 00:02:04 -0500

    Remove commets and use fs lib

--- src/lib/get-pages.js
+++ src/lib/get-pages.js
@@ -1,16 +1,14 @@
-const fs = require("fs");
 const globby = require("globby");
-const { promisify } = require("util");
 
-// import updatedFiles from "./updated-files";
-
-const regex = ["./pages/**/*.mdx", "!./pages/index.mdx", "!./pages/articles.mdx"];
-const readFile = promisify(fs.readFile);
+const regex = [
+  "./pages/**/*.mdx",
+  "!./pages/index.mdx",
+  "!./pages/articles.mdx"
+];
+const { readFile } = require("./fs");
 
 async function getPages() {
   const paths = await globby(regex);
-  // const updated = await updatedFiles();
-  // filter somehow
   return await Promise.all(
     paths.map(async path => ({
       content: await readFile(path, "utf8"),
