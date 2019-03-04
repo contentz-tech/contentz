134dd63ccaa4b5556333272c6d3758e2b66cf8c2
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-04 16:15:23 -0500

    Wrap Open Graph in proper HTML tags and set encoding

--- src/components/open-graph.js
+++ src/components/open-graph.js
@@ -2,41 +2,61 @@ const { jsx } = require("@emotion/core");
 
 function OpenGraph({ title, description }) {
   return jsx(
-    "div",
-    {
-      css: {
-        height: "100vh",
-        fontSize: "100px",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: "1em",
-        boxSizing: "border-box",
-        fontFamily:
-          "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
-      }
-    },
+    "html",
+    null,
     jsx(
-      "div",
-      {
-        id: "title",
-        css: { fontWeight: "bold", letterSpacing: "-0.028em", margin: "1em 0" }
-      },
-      title
+      "head",
+      null,
+      jsx("meta", {
+        httpEquiv: "Content-Type",
+        content: "text/html; charset=utf-8"
+      })
     ),
     jsx(
-      "div",
-      {
-        id: "description",
-        css: {
-          fontWeight: 200,
-          borderLeft: "3px solid black",
-          boxSizing: "border-box",
-          paddingLeft: "calc(0.5em - 3px)",
-          margin: "-1em 0 1em -0.5em"
-        }
-      },
-      description
+      "body",
+      null,
+      jsx(
+        "div",
+        {
+          css: {
+            height: "100vh",
+            fontSize: "100px",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            padding: "1em",
+            boxSizing: "border-box",
+            fontFamily:
+              "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+          }
+        },
+        jsx(
+          "div",
+          {
+            id: "title",
+            css: {
+              fontWeight: "bold",
+              letterSpacing: "-0.028em",
+              margin: "1em 0"
+            }
+          },
+          title
+        ),
+        jsx(
+          "div",
+          {
+            id: "description",
+            css: {
+              fontWeight: 200,
+              borderLeft: "3px solid black",
+              boxSizing: "border-box",
+              paddingLeft: "calc(0.5em - 3px)",
+              margin: "-1em 0 1em -0.5em"
+            }
+          },
+          description
+        )
+      )
     )
   );
 }
