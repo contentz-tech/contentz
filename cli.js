6cfc302a346114904a09ff15029ea7c0ff5d4ed5
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-04 16:19:02 -0500

    Update CLI file to create a proper CLI tool with commands and help

--- cli.js
+++ cli.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
-require("./src/index.js");
+const meow = require("meow");
+const chalk = require("chalk");
+
+const pkg = require("./package.json");
+
+const cli = meow(
+  `${chalk.white("Usage")}
+  $ ${chalk.cyan("contentz build")}
+  $ ${chalk.cyan("contentz social <path>")}
+`,
+  {
+    description: `${chalk.cyan("Contentz")} - ${pkg.description}`
+  }
+);
+
+const [cmd, ...file] = cli.input;
+
+if (!cmd) cli.showHelp(0);
+
+const commands = {
+  build: require("./src/build"),
+  social: require("./src/social"),
+  write: () => {},
+  page: () => {}
+}
+
+const command = commands[cmd];
+command(file);
