3baf0a88ef00400b08dc97595c38f80c49130b6a
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-07 13:36:59 -0500

    Remove background image

--- src/components/card.js
+++ src/components/card.js
@@ -5,8 +5,6 @@ function Card(props) {
     css: {
       boxShadow: "rgba(0, 0, 0, 0.12) 0px 2px 5px 0px",
       backgroundColor: "white",
-      backgroundImage: "radial-gradient( circle, #d7d7d7, #d7d7d7 1px, #fff 1px, #fff )",
-      backgroundSize: "28px 28px",
       borderRadius: "5px",
       transition: "all 0.2s ease 0s",
       userSelect: "none",
@@ -14,8 +12,6 @@ function Card(props) {
       padding: "1em 2em",
       "@media (prefers-color-scheme: dark)": {
         backgroundColor: "#151515",
-        backgroundImage: "radial-gradient(circle, #3a3a3a, #3a3a3a 1px, #151515 1px, #151515 )",
-        boxShadow: "rgba(255, 255, 255, 0.12) 0px 2px 5px 0px",
         color: "white"
       },
       "@media (max-width: 720px)": {
