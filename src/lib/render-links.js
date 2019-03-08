72534eec3e55cc8ea40e6e73c0ff2a00d5f15bca
Sergio Xalambrí <hello@sergiodxa.com>
2019-03-08 14:27:07 -0500

    Provide Intl to links

--- src/lib/render-links.js
+++ src/lib/render-links.js
@@ -5,9 +5,11 @@ const { join } = require("path");
 
 const { makeDir, writeFile } = require("./fs");
 const { checkCache } = require("./cache");
+const i18n = require("./i18n");
 
 const LinksPage = require("../components/links");
 const Document = require("../components/document");
+const { IntlProvider } = require("../components/i18n");
 
 async function writeContent(html) {
   const finalPath = join(process.cwd(), "./public", "/links", "/");
@@ -20,13 +22,24 @@ async function render(links, config) {
   if (
     (await checkCache("links.yml", JSON.stringify(links))) &&
     (await checkCache("config.yml", JSON.stringify(config)))
-  )
+  ) {
     return true;
+  }
+
+  const messages = await i18n();
 
   try {
     const html = renderStylesToString(
       ReactDOMServer.renderToStaticMarkup(
-        jsx(Document, { config, path: "links.mdx" }, jsx(LinksPage, { config, links }))
+        jsx(
+          IntlProvider,
+          { locale: config.language || config.lang || "en", messages },
+          jsx(
+            Document,
+            { messages, config, path: "links.mdx" },
+            jsx(LinksPage, { messages, config, links })
+          )
+        )
       )
     );
     await writeContent(`<!DOCTYPE html>${html}`);
