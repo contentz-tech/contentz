e56e2b07f02c0020c56a0b1a8f643cfb7bd517ef
Sergio Xalambrí <hello@sergiodxa.com>
2019-04-02 12:03:08 -0500

    Update cli.js to dynamically require commands

--- cli.js
+++ cli.js
@@ -1,36 +1,49 @@
 #!/usr/bin/env node
 const meow = require("meow");
 const chalk = require("chalk");
+const { join } = require("path");
 
 const pkg = require("./package.json");
 
-const cli = meow(
-  `${chalk.white("Usage")}
-  $ ${chalk.cyan("contentz build")} Build the whole website
-  $ ${chalk.cyan(
-    "contentz social <path>"
-  )} Generate the social images for the given files
-  $ ${chalk.cyan(
-    "contentz write <path>"
-  )} Generate a base article inside "/articles" with the current date
-  $ ${chalk.cyan("contentz help")} Show this message
-`,
-  {
-    description: `${chalk.cyan("Contentz")} - ${pkg.description}`
+async function main() {
+  const cli = meow(
+    `${chalk.white("Usage")}
+    $ ${chalk.cyan("contentz build")} Build the whole website
+    $ ${chalk.cyan(
+      "contentz social <path>"
+    )} Generate the social images for the given files
+    $ ${chalk.cyan(
+      "contentz write <path>"
+    )} Generate a base article inside "/articles" with the current date and non published
+    $ ${chalk.cyan("contentz help")} Show this message
+  `,
+    {
+      description: `${chalk.cyan("Contentz")} - ${pkg.description}`
+    }
+  );
+
+  let [cmd = "build", ...files] = cli.input;
+  cmd = cmd.toLowerCase();
+
+  switch (cmd) {
+    case "help": {
+      return cli.showHelp(0);
+    }
+    case "build":
+    case "social":
+    case "write": {
+      const command = require(join("./src", cmd));
+      await command(files);
+      return;
+    }
+    default: {
+      console.error(`Command ${cmd} is not supported.`);
+      cli.showHelp(1);
+    }
   }
-);
+}
 
-const [cmd, ...files] = cli.input;
-
-if (cmd === "help") cli.showHelp(0);
-if (!cmd) require("./src/build")(files);
-
-const commands = {
-  build: require("./src/build"),
-  social: require("./src/social"),
-  write: require("./src/write"),
-  page: () => {}
-};
-
-const command = commands[cmd];
-command(files);
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
